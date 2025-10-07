ff82f5e5903112d49aeabe41e7012ab2a9776319
saideva15536 <saideva15536@gmail.com>
2025-10-07 15:39:00 +0000

    commit

--- app/src/main/java/com/example/campusvibe/MainActivity.kt
+++ app/src/main/java/com/example/campusvibe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.campusvibe
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.campusvibe.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -28,16 +29,13 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setupWithNavController(navController)
 
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.addPostFragment -> {
-                    navController.navigate(R.id.addPostFragment)
-                    true
-                }
-                else -> {
-                    // Let the NavController handle the other menu items
-                    navController.navigate(item.itemId)
-                    true
-                }
+            if (item.itemId == R.id.addPostFragment) {
+                navController.navigate(R.id.addPostFragment)
+                // Return true to indicate the item selection has been handled
+                true
+            } else {
+                // Use NavigationUI to handle other menu items
+                NavigationUI.onNavDestinationSelected(item, navController)
             }
         }
     }
