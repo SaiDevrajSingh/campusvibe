6f9add2363858d4786af89a0f8090f13521edcdb
saideva15536 <saideva15536@gmail.com>
2025-11-01 04:51:38 +0000

    commit

--- app/src/main/java/com/example/campusvibe/fragments/MyPostFragment.kt
+++ app/src/main/java/com/example/campusvibe/fragments/MyPostFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.campusvibe.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.campusvibe.Models.Post
+import com.example.campusvibe.adapter.MyPostRvAdapter
+import com.example.campusvibe.databinding.FragmentMyPostBinding
+import com.example.campusvibe.utils.SupabaseClient
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.postgrest.postgrest
+import kotlinx.coroutines.launch
 
 class MyPostFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyPostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_post, container, false)
+        binding = FragmentMyPostBinding.inflate(inflater, container, false)
+        val postList = ArrayList<Post>()
+        val adapter = MyPostRvAdapter(requireContext(), postList)
+        binding.rv.layoutManager= StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rv.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val currentUserId = SupabaseClient.client.auth.currentUserOrNull()?.id
+            if (currentUserId != null) {
+                val posts = SupabaseClient.client.postgrest["posts"].select {
+                    filter {
+                        eq("userId", currentUserId)
+                    }
+                }.decodeList<Post>()
+                postList.addAll(posts)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/campusvibe/fragments/MyReelsFragment.kt
+++ app/src/main/java/com/example/campusvibe/fragments/MyReelsFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.campusvibe.R
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.campusvibe.Models.Reel
+import com.example.campusvibe.adapter.MyReelAdapter
+import com.example.campusvibe.databinding.FragmentMyReelsBinding
+import com.example.campusvibe.utils.SupabaseClient
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.postgrest.postgrest
+import kotlinx.coroutines.launch
 
 class MyReelsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyReelsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_reels, container, false)
+        binding = FragmentMyReelsBinding.inflate(inflater, container, false)
+        val reelList = ArrayList<Reel>()
+        val adapter = MyReelAdapter(requireContext(), reelList)
+        binding.rv.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rv.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            val currentUserId = SupabaseClient.client.auth.currentUserOrNull()?.id
+            if (currentUserId != null) {
+                val reels = SupabaseClient.client.postgrest["reels"].select {
+                    filter {
+                        eq("userId", currentUserId)
+                    }
+                }.decodeList<Reel>()
+                reelList.addAll(reels)
+                adapter.notifyDataSetChanged()
+            }
+        }
+
+        return binding.root
     }
 }
